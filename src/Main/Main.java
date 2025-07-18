760d82c2a9bdd1816964f8ad17a5a2af007d129a
RafaMoraes2004 <rafamoraescosta01@gmail.com>
2025-07-18 12:19:47 -0300

    feat: implementações novas(Menu e reader)
    Implementação do Util reader, para ler e printar o arquivo desejado e também implementação do menu
    para utilização do usuario(Ainda sem as funções logicas funcionarem).

---
+++ src/Main/Main.java
@@ -0,0 +1,66 @@
+package Main;
+
+import Main.Utils.UtilReadPrint;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        UtilReadPrint rp = new UtilReadPrint();
+
+        String path = "/home/rafael/Área de trabalho/UDEMY-JAVA/desafioCadastro/data/formulario.txt";
+        rp.printFile(path);
+
+        menu(sc);
+
+        System.out.println("Fechando programa....");
+    }
+
+    public static void menu(Scanner sc) {
+        int in;
+
+        do {
+            System.out.println("---------------------- MENU --------------------------");
+            System.out.println();
+            System.out.println("1. Cadastrar um novo pet");
+            System.out.println("2. Alterar os dados do pet cadastrado");
+            System.out.println("3. Deletar um pet cadastrado");
+            System.out.println("4. Listar todos os pets cadastrados");
+            System.out.println("5. Listar pets por algum critério (idade, nome, raça)");
+            System.out.println("6. Sair");
+            System.out.println();
+            System.out.println("Digite a opção que deseja: ");
+
+            try {
+                in = sc.nextInt();
+
+                switch (in) {
+                    case 1 -> System.out.println("1 Cadastrar ");
+                    case 2 -> System.out.println("2 alterar");
+                    case 3 -> System.out.println("3 deletar");
+                    case 4 -> System.out.println("4 listar");
+                    case 5 -> System.out.println("5 listar pets");
+                    case 6 -> System.out.println("6 sair");
+
+                    default -> {
+                        System.out.println("Opção invalida");
+                        sc.nextLine();
+                        in = 0;
+
+                    }
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Digite um valor valido");
+                sc.nextLine();
+                in = 0;
+            }
+
+        } while (in != 6);
+
+    }
+}
---
+++ src/Main/Utils/UtilReadPrint.java
@@ -0,0 +1,26 @@
+package Main.Utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class UtilReadPrint {
+
+
+    public void printFile(String path){
+
+    try (BufferedReader br = new BufferedReader(new FileReader(path))){
+
+        String line = br.readLine();
+        while (line != null){
+
+            System.out.println(line);
+            line = br.readLine();
+        }
+        
+    }catch(IOException e){
+        System.out.println("Error: " + e.getMessage());
+    }
+}
+
+}
